53702f6ec392a895c4b697f79a82a24609c39f95
Beomsuk Seo <beomsukbrendanseo@gmail.com>
2025-09-21 15:58:49 -0700

    Added normalization + general preprocessing framework to data_pipeline.

---
+++ src/data_pipeline.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import os
+
+IMG_SIZE = (224, 224) #standard size, e.g. ResNet50
+BATCH_SIZE = 32
+SEED = 123
+DATA_DIR = os.path.join("../data", "raw")
+
+def get_dataset(subset):
+    """
+    Create tf.data.Dataset from specified directory.
+
+    Args:
+        subset (str): "train", "test", or "val"
+    
+    Returns:
+        tf.data.Dataset
+    """
+    dataset = tf.keras.utils.image_dataset_from_directory(
+        os.path.join(DATA_DIR, subset),
+        labels = "inferred", # based on folder structure
+        label_mode = "categorical",
+        image_size = IMG_SIZE, #resizing image (preprocessing)
+        batch_size = BATCH_SIZE,
+        shuffle = True if subset == "train" else False, #only shuffling training set
+        seed = SEED
+    )
+    return dataset
+
+# normalization layer
+normalization_layer = tf.keras.layers.Rescaling(1. / 255)
+
+def preprocess(dataset):
+    #TODO: DATA AUGMENTATION
+    #TODO: Caching / Prefetching
+
+    """
+    Preprocessing (normalization, augmentation, etc.)
+    """
+    # normalize
+    dataset = dataset.map(lambda x, y: (normalization_layer(x), y))
+    return dataset
+
+train_ds = preprocess(get_dataset("train"))
+test_ds = preprocess(get_dataset("test"))
+val_ds = preprocess(get_dataset("val"))
+
