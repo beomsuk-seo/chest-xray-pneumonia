4ff1fd8b7073d24826a31656ff49100994b7014f
Beomsuk Seo <beomsukbrendanseo@gmail.com>
2025-09-21 18:45:33 -0700

    Updated gitignore for cache, added base ResNet50 model to train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,5 +1,23 @@
 import tensorflow as tf
 from preprocessing_pipeline import load_datasets
 
+#params
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 32
+SEED = 123
+NUM_CLASSES = 3
+EPOCHS = 10
+LEARNING_RATE = 1e-4
+
 # load preprocessed datasets (see preprocesing_pipeline.py)
 train_ds, test_ds, val_ds = load_datasets()
+
+#model: ResNet50
+base_model = tf.keras.applications.ResNet50(
+    input_shape = IMG_SIZE + (3,), # (224, 224, 3)
+    include_top = False, # exclude original resnet classifier
+    weights = 'imagenet' # transfer learning
+)
+
+#freeze base model initially
+base_model.trainable = False
